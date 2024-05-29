ff8cf6d2b1e7408008c12a59265f5ba391fa7f05
paulo_sneto <paulo_sneto@yahoo.com>
2024-05-29 16:39:28 -0300

    feat: create WalletPicpayApplication.java

---
+++ src/main/java/br/com/wallet/picpay/WalletPicpayApplication.java
@@ -0,0 +1,13 @@
+package br.com.wallet.picpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletPicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletPicpayApplication.class, args);
+	}
+
+}
