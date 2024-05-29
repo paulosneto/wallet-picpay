27372ef337287bb4628572d8c7eafb51b3fe3714
paulo_sneto <paulo_sneto@yahoo.com>
2024-05-29 16:39:37 -0300

    feat: create WalletPicpayApplicationTests.java

---
+++ src/test/java/br/com/wallet/picpay/WalletPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wallet.picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletPicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
