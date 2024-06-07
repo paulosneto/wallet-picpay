8c15136a5fc188daef9a679c59bd3ab02bec80ca
paulo_sneto <paulo_sneto@yahoo.com>
2024-06-07 15:29:59 -0300

    feat: create WalletService.java

--- src/main/java/br/com/wallet/picpay/services/WalletService.java
+++ src/main/java/br/com/wallet/picpay/services/WalletService.java
@@ -2,26 +2,37 @@ package br.com.wallet.picpay.services;
 
 import br.com.wallet.picpay.domain.Wallet;
 import br.com.wallet.picpay.dto.WalletDTO;
+import br.com.wallet.picpay.exceptions.WalletExceptions;
 import br.com.wallet.picpay.repositories.WalletRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class WalletService {
 
-    @Autowired
-    private WalletRepository repository;
+
+    private final WalletRepository repository;
 
     public WalletService(WalletRepository repository){
         this.repository = repository;
     }
 
     public Wallet insert(WalletDTO dto){
-        Wallet w = this.repository.findById(dto.cpfCnpj()).orElseThrow(() -> {new Exception("User not found");
-            return null;
-        });
+        Optional<Wallet> w = this.repository.findByCpfCnpjOrEmail(dto.cpfCnpj(), dto.email());
 
-               return this.repository.save(w);
+        if(w.isPresent()){
+             throw new WalletExceptions("Usuário já existente");
+        }else{
+            Wallet wallet = new Wallet(dto);
+           return this.repository.save(wallet);
+        }
+    }
+
+    public List<Wallet> getAll(){
+        return this.repository.findAll();
     }
 
 }
