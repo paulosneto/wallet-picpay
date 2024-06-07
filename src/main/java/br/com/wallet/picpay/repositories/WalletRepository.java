f5841aac7aee2fb1fea86bf228803f0c33a48941
paulo_sneto <paulo_sneto@yahoo.com>
2024-06-07 15:06:11 -0300

    feat: create WalletRepository.java

--- src/main/java/br/com/wallet/picpay/repositories/WalletRepository.java
+++ src/main/java/br/com/wallet/picpay/repositories/WalletRepository.java
@@ -3,5 +3,8 @@ package br.com.wallet.picpay.repositories;
 import br.com.wallet.picpay.domain.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WalletRepository extends JpaRepository<Wallet, String> {
+import java.util.Optional;
+
+public interface WalletRepository extends JpaRepository<Wallet, Integer> {
+    Optional<Wallet> findByCpfCnpjOrEmail(String cpfcnpj, String email);
 }
