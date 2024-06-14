4d99b82b5c4284a85f20d758253519117fd19a95
paulo_sneto <paulo_sneto@yahoo.com>
2024-06-14 15:30:20 -0300

    fix: update WalletDTO.java

--- src/main/java/br/com/wallet/picpay/dto/WalletDTO.java
+++ src/main/java/br/com/wallet/picpay/dto/WalletDTO.java
@@ -1,12 +1,14 @@
 package br.com.wallet.picpay.dto;
 
 import br.com.wallet.picpay.domain.WalletType;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 
-public record WalletDTO (@NotBlank String cpfCnpj,
+public record WalletDTO (@NotNull @DecimalMin("0.01") BigDecimal balance,
+                         @NotBlank String cpfCnpj,
                          @NotBlank String email,
                          @NotBlank String name,
                          @NotBlank String password,
