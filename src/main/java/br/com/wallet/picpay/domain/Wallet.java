97e5c316cbdfc6a8d4acea8c17daad5016fda6f5
paulo_sneto <paulo_sneto@yahoo.com>
2024-06-14 15:30:08 -0300

    fix: update Wallet.java

--- src/main/java/br/com/wallet/picpay/domain/Wallet.java
+++ src/main/java/br/com/wallet/picpay/domain/Wallet.java
@@ -31,10 +31,9 @@ public class Wallet {
     @ManyToOne
     @JoinColumn(name = "wallet_type_id")
     private WalletType walletType;
-    //private int id_wallet_type;
 
     public Wallet(WalletDTO dto){
-        //this.balance = dto.balance();
+        this.balance = dto.balance();
         this.cpfCnpj = dto.cpfCnpj();
         this.email = dto.email();
         this.name = dto.name();
@@ -42,4 +41,37 @@ public class Wallet {
         this.walletType = dto.walletType().get();
     }
 
+    // Verifica o tipo de usuario sendo do tipo USER para efetuar transação
+    public boolean isTransferAllowedForWalletType(){
+        //return this.walletType.getDescription().toLowerCase().equals(WalletType.Enum.USER.get());
+        WalletType tt = WalletType.Enum.USER.get();
+        WalletType tet = this.walletType;
+
+        if(this.walletType.getId() ==  WalletType.Enum.USER.get().getId() && this.walletType.getDescription().equals(WalletType.Enum.USER.name())){
+            return true;
+        }else{
+            return this.walletType.equals(WalletType.Enum.USER.get());
+        }
+
+        /*boolean vt = WalletType.Enum.USER.name() == this.walletType.getDescription().toString();
+        WalletType valorW = WalletType.Enum.USER.get();
+        boolean bl = WalletType.Enum.USER.get().equals(this.walletType);*/
+
+    }
+
+    // Verifica o saldo da pessoa para fazer a transferencia
+    public boolean isBalanceEqualOrGreatherThan(BigDecimal value){
+        return this.balance.doubleValue() >= value.doubleValue();
+    }
+
+
+    // Diminui o valor pago do saldo de conta de quem está enviado
+    public void debit(BigDecimal value) {
+        this.balance = this.balance.subtract(value);
+    }
+
+    // Adiciona o valor recebido ao saldo da conta
+    public void credit(BigDecimal value){
+        this.balance = this.balance.add(value);
+    }
 }
