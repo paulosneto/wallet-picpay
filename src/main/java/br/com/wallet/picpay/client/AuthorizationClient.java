33015959c6b4f5a0d722c394fb62f5b9a39b05b6
paulo_sneto <paulo_sneto@yahoo.com>
2024-06-14 15:20:43 -0300

    feat: create AuthorizationClient.java

---
+++ src/main/java/br/com/wallet/picpay/client/AuthorizationClient.java
@@ -0,0 +1,18 @@
+package br.com.wallet.picpay.client;
+
+
+import br.com.wallet.picpay.dto.AuthorizationClientDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(
+        name = "AuthorizationClient",
+        url = "${client.authorization.service.url}")
+public interface AuthorizationClient {
+
+    @GetMapping
+    ResponseEntity<AuthorizationClientDTO> isAuthorized();
+
+
+}
