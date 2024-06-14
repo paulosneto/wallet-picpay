bb6626b61f4eab6118534a24ec46d2134b46fe7d
paulo_sneto <paulo_sneto@yahoo.com>
2024-06-14 15:31:23 -0300

    fix: update WalletPicpayApplication.java

--- src/main/java/br/com/wallet/picpay/WalletPicpayApplication.java
+++ src/main/java/br/com/wallet/picpay/WalletPicpayApplication.java
@@ -2,7 +2,10 @@ package br.com.wallet.picpay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 
+@EnableFeignClients
 @SpringBootApplication
 public class WalletPicpayApplication {
 
